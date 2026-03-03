eb8016aee6c8bf32c41ba70a3f383182182c505d
Jorge Beltran <jebeltran@ucdavis.edu>
2026-03-02 17:56:11 -0800

    Completed 2.6, Creating the initial phonebook app. It only takes names.

---
+++ Part 2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const newNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  } 
+
+  const onNewPersonFormSubmit = (event) => {
+    event.preventDefault()
+    console.log("Adding person's name ", newName)
+    setPersons(persons.concat({ name: newName }))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onNewPersonFormSubmit}>
+        <div>
+          name: <input value={newName} onChange={newNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <table>
+        <tbody>
+          {persons.map((person) => <tr key={person.name}><td>{person.name}</td></tr>) }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
