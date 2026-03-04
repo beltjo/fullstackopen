fd7dbe5b4920604db15faacc399d7d8a2831e3d7
Jorge Beltran <jebeltran@ucdavis.edu>
2026-03-03 17:12:09 -0800

    Completed 2.11, added json-server and axios get calls.

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, filterChange}) => {
   return (
@@ -32,7 +33,7 @@ const Phonebook = ({phonebook}) => {
   return (      
         <table>
           <tbody>
-            {phonebook.map((person) => <tr key={person.name}><td>{person.name}</td><td>{person.phoneNumber}</td></tr>) }
+            {phonebook.map((person) => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>) }
           </tbody>
         </table>
       )
@@ -40,15 +41,20 @@ const Phonebook = ({phonebook}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [filter, setFilter] = useState('')
+  
+  useEffect(() => {
+    console.log("Starting effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  }, [])
 
   const newNameChange = (event) => {
     console.log(event.target.value)
@@ -73,7 +79,7 @@ const App = () => {
       window.alert(`${newName} is already added to the phonebook.`)
     }
     else {
-      setPersons(persons.concat({ name: newName, phoneNumber: newPhoneNumber}))
+      setPersons(persons.concat({ name: newName, number: newPhoneNumber, id:persons.length + 1}))
       setNewName("")
       setNewPhoneNumber("")
     }    
