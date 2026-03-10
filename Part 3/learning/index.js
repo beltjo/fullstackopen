bfad9c0825903ff55108ac9ad712b814b2570c0a
Jorge Beltran <jebeltran@ucdavis.edu>
2026-03-10 10:38:25 -0700

    Completed 3.1, set up phonebookBackend and get persons api.

--- Part 3/learning/index.js
+++ Part 3/learning/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [  {
     id: "1",
@@ -14,15 +15,25 @@ let notes = [  {
     id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
+  },
+  {
+    id:"4",
+    content: "Hello, world",
+    important: false
   }
 ]
 
-
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'content-Type': 'application/json'})
-    response.end(JSON.stringify(notes))
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World! </h1>")
 })
 
+app.get("/api/notes", (request, response) => {
+    response.json(notes)
+})
+
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
+console.log('Done')
---
+++ Part 3/phonebookBackend/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+
+let people = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const app = express()
+
+app.get("/", (request, response) => {
+    response.end('<h1>Hello, click on a link to test a connection</h1><a href="../api/persons">See all people</a>')
+})
+
+
+app.get("/api/persons", (request, response) => {
+    response.json(people)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
