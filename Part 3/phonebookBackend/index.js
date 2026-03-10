1ae9fa8d25bd9efac17fbd1a4463f711d3b01ca9
Jorge Beltran <jebeltran@ucdavis.edu>
2026-03-10 12:36:29 -0700

    Completed 3.4, added delete individual person endpoint

--- Part 3/phonebookBackend/index.js
+++ Part 3/phonebookBackend/index.js
@@ -47,6 +47,16 @@ app.get("/api/persons/:id", (request, response) => {
 
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    
+    people = people.filter((person) => person.id !== id)
+
+    return response.status(204)
+
+})
+
+
 
 app.get("/info", (request, response) => {
     const timestamp = new Date(Date.now())
