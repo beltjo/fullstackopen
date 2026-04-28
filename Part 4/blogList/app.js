73955dc02f9f2778d5da1612f2100dd0ab0c9cab
Jorge Beltran <jebeltran@ucdavis.edu>
2026-04-27 17:41:16 -0700

    Completed 4.15, Added create new user api.

--- Part 4/blogList/app.js
+++ Part 4/blogList/app.js
@@ -2,6 +2,7 @@ const middleware = require('./utils/middleware')
 const express = require('express')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogRouter')
+const userRouter = require('./controllers/userRouter')
 const config = require('./utils/config')
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(middleware.requestLogger)
 
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
