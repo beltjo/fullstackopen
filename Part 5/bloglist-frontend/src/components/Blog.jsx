5ede0ec0d960184fa5007d323231b3dba04c0c8a
Jorge Beltran <jebeltran@ucdavis.edu>
2026-05-14 16:49:54 -0700

    Added style to the blogs.

--- Part 5/bloglist-frontend/src/components/Blog.jsx
+++ Part 5/bloglist-frontend/src/components/Blog.jsx
@@ -1,12 +1,19 @@
 import {useState} from 'react'
 
 const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
 
   const [detailsVisible, setDetailsVisible] = useState(false)
 
   const buttonText = detailsVisible ? 'hide' : 'show'
   return (
-  <div>
+  <div style={blogStyle}>
     {blog.title} {blog.author}
     <button onClick={ () => { setDetailsVisible(!detailsVisible) }}>{buttonText}</button>
     { detailsVisible && (
