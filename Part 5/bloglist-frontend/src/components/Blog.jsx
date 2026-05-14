c0654e385d21f77587d27bfaae8e08d172817078
Jorge Beltran <jebeltran@ucdavis.edu>
2026-05-14 16:36:46 -0700

    Completed 5.7, Added show/hide button for the details of each blog.

--- Part 5/bloglist-frontend/src/components/Blog.jsx
+++ Part 5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
+import {useState} from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const buttonText = detailsVisible ? 'hide' : 'show'
+  return (
   <div>
     {blog.title} {blog.author}
+    <button onClick={ () => { setDetailsVisible(!detailsVisible) }}>{buttonText}</button>
+    { detailsVisible && (
+      <>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button> </div>
+        <div>{blog.author}</div>
+      </>
+    )}
   </div>  
-)
+  )
+}
 
 export default Blog
